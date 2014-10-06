af76e0dabd6b92dbceebd38cfe7f754b2bbe591c
Joao Guerreiro <jmdguerreiro@gmail.com>
2014-10-06 22:19:30 +0200

    tested authentication (OAuth) agains't twitter api

---
+++ src/main/groovy/se/svt/wordcloud/resource/twitter/OAuthSettings.groovy
@@ -0,0 +1,11 @@
+package se.svt.wordcloud.resource.twitter
+
+
+class OAuthSettings {
+
+    public static final CONSUMER_KEY = "zpAgon0QLdL3Txx6m16TdIZZA"
+    public static final CONSUMER_SECRET = "7swVa1RHjuVXks1ueTIofNcPq5KkSC0SP4x3IoqQQwt5VuM1cG"
+    public static final ACCESS_TOKEN = "2811199503-K6yYylFhqs6YEQHG3x9BhVexwvMZCP1lCWKVREL"
+    public static final SECRET_TOKEN = "vYMz2lTcscJaZ23pF6yyV9BhEAPsIKLKrdqcXfeuHomz2"
+
+}
